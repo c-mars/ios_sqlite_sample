12b43bae185f02351af8cab4ad4706e983f02665
Constantine Mars <constantine.mars@gmail.com>
2016-03-24 21:08:01 +0200

    Initial Commit

---
+++ SQLiteSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SQLiteSample
+//
+//  Created by Constantine Mars on 3/24/16.
+//  Copyright © 2016 Constantine Mars. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SQLiteSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SQLiteSample
+//
+//  Created by Constantine Mars on 3/24/16.
+//  Copyright © 2016 Constantine Mars. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
